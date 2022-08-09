2509620ab016b8c9ca4b6126a47687047fb165b9
Cristiano <cristiano.oliveira@incognia.com>
2022-08-09 15:30:14 -0300

    chore(Package.swift): updates version to 6.7.1

--- Package.swift
+++ Package.swift
@@ -13,17 +13,17 @@ let package = Package(
         .binaryTarget(
             name: "IncogniaCore",
             url: "https://repo.incognia.com/ios/6.7.0/IncogniaCore-6.7.0.zip",
-            checksum: "6408b95a4f9a07a93ba10b63a8cb97532c5699bfce6f95e45c1257effcc0b7c5"
+            checksum: "f7fe4bb2cc6bfef85db6de9f044dd16cf5aafe5f1f86e0c64d449a58a3320e2f"
         ),
         .binaryTarget(
             name: "Incognia",
             url: "https://repo.incognia.com/ios/6.7.0/Incognia-6.7.0.zip",
-            checksum: "c2c58a0424543e5bd5c467a99bbb6d13aaa0d17f8744632ceae6a4555d86db49"
+            checksum: "9d559fff5fecbb143cb45756ef2321ca1e2b58dec634e3c00c381c9e005a3afb"
         ),
         .binaryTarget(
             name: "IncogniaTrial",
             url: "https://repo.incognia.com/ios/6.7.0/IncogniaTrial-6.7.0.zip",
-            checksum: "bbc6bfc9e4e7833a08bbe9cd01c64f6be95aa70769bc0a4b18050eba8b583b7e"
+            checksum: "aa6d4c12861edeb8754001c78bc9b12fcdc85525564ef0d3528afbebaa284f21"
         ),
     ]
 )
