524054a800267785f8fbbc6709518ef090f3bb2b
Douglas Soares <douglas.soares@incognia.com>
2023-09-12 17:52:41 -0300

    Fix url path for version 6.11.5

--- Package.swift
+++ Package.swift
@@ -12,17 +12,17 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IncogniaCore",
-            url: "https://repo.incognia.com/ios/6.11.4/IncogniaCore-6.11.5.zip",
+            url: "https://repo.incognia.com/ios/6.11.5/IncogniaCore-6.11.5.zip",
             checksum: "704d6cc8295786b01e5702ba6f5b8a80b4db010f3a485152f9bfc58a28c8e933"
         ),
         .binaryTarget(
             name: "Incognia",
-            url: "https://repo.incognia.com/ios/6.11.4/Incognia-6.11.5.zip",
+            url: "https://repo.incognia.com/ios/6.11.5/Incognia-6.11.5.zip",
             checksum: "d0359a6b4949e76219dfca361d64050e8ed2fd9dd3d612cdb2ef965d36cb0422"
         ),
         .binaryTarget(
             name: "IncogniaTrial",
-            url: "https://repo.incognia.com/ios/6.11.4/IncogniaTrial-6.11.5.zip",
+            url: "https://repo.incognia.com/ios/6.11.5/IncogniaTrial-6.11.5.zip",
             checksum: "fed48dbf05a9b36ba40f823491643f50ef34f639414cc76852bc87d4422a0c3c"
         ),
     ]
