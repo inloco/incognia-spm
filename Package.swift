96c2d778a02077d07eeb77718493d683c1766c58
Lucas Cardoso <lucas.cardoso@incognia.com>
2023-09-28 18:17:24 -0300

    chore(Package.swift): updates package checksum

--- Package.swift
+++ Package.swift
@@ -12,18 +12,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IncogniaCore",
-            url: "https://repo.incognia.com/ios/6.11.5/IncogniaCore-6.11.5.zip",
-            checksum: "704d6cc8295786b01e5702ba6f5b8a80b4db010f3a485152f9bfc58a28c8e933"
+            url: "https://repo.incognia.com/ios/6.11.6/IncogniaCore-6.11.6.zip",
+            checksum: "3b531b801ed03d4b353566eb5d31d4bc9da65bc3beadb192f0f845692e1ae1b6"
         ),
         .binaryTarget(
             name: "Incognia",
-            url: "https://repo.incognia.com/ios/6.11.5/Incognia-6.11.5.zip",
-            checksum: "d0359a6b4949e76219dfca361d64050e8ed2fd9dd3d612cdb2ef965d36cb0422"
+            url: "https://repo.incognia.com/ios/6.11.6/Incognia-6.11.6.zip",
+            checksum: "cad8ecc4aa63675d41ccb4a3cb27e22734c942ad1fb2bf53bd2ed38c0757cabc"
         ),
         .binaryTarget(
             name: "IncogniaTrial",
-            url: "https://repo.incognia.com/ios/6.11.5/IncogniaTrial-6.11.5.zip",
-            checksum: "fed48dbf05a9b36ba40f823491643f50ef34f639414cc76852bc87d4422a0c3c"
+            url: "https://repo.incognia.com/ios/6.11.6/IncogniaTrial-6.11.6.zip",
+            checksum: "cc1444735141fed73ce1d755c60e54606c678b2c356e57f7289a373c43b646d3"
         ),
     ]
 )
