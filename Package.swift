0f8762c3f43016ad1d6b1d47c2f35c2a0a771b6d
Pedro Atanasio <pedro.atanasio@incognia.com>
2025-03-13 09:48:33 -0300

    Release 6.18.2

--- Package.swift
+++ Package.swift
@@ -33,18 +33,18 @@ let package = Package(
         ),
         .binaryTarget(
             name: "IncogniaCore",
-            url: "https://repo.incognia.com/ios/6.18.1/IncogniaCore-6.18.1.zip",
-            checksum: "9f89d53281d5febaceb5d0e69f3f7c9d2ba8ee3c4085dab87a918f01c684aa14"
+            url: "https://repo.incognia.com/ios/6.18.2/IncogniaCore-6.18.2.zip",
+            checksum: "872dd5889d68bf40d3c69f193b0ef81b9617503e41c16a753c66c65feabe8f86"
         ),
         .binaryTarget(
             name: "Incognia",
-            url: "https://repo.incognia.com/ios/6.18.1/Incognia-6.18.1.zip",
-            checksum: "1a74370f0fc9daf77ae2470686b3e4cb03e6c80ad8895869269486f3fd598e9b"
+            url: "https://repo.incognia.com/ios/6.18.2/Incognia-6.18.2.zip",
+            checksum: "98bc3c11a2ae9d50ed6100ddd12abdcbd8f14df5c825fef1dbd0fe4bcffe034c"
         ),
         .binaryTarget(
             name: "IncogniaTrial",
-            url: "https://repo.incognia.com/ios/6.18.1/IncogniaTrial-6.18.1.zip",
-            checksum: "e47b36ee36d8b2c5b27d0db87b2e720d6e3cad4df2b9ec8a2986b8d21af0dc56"
+            url: "https://repo.incognia.com/ios/6.18.2/IncogniaTrial-6.18.2.zip",
+            checksum: "3009bf480bc1f10700c3550a036f97551af167cc7b8e1cffe0e2854d272af97d"
         ),
     ]
 )
